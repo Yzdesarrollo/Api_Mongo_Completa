8eb54fe0b516f6339c41194bdd048e4c43ec4e6a
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 17:02:27 -0500

    creando variables de entorno en config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 9000,
+    db: process.env.MONGODB || 'mongodb://localhost:27017/productsyzm'
+}
--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = process.env.PORT || 9000;
+const config = require('./config');
 
 // server connect
-mongoose.connect('mongodb://localhost:27017/productsyzm', (error, res) => {
+mongoose.connect(config.db, (error, res) => {
     if (error) {
         return console.log(`Error al conectar a la base de datos: ${error}`)
     }
     console.log('Conexion establecida a la BD');
 
-    const server = app.listen(port, () => {
-        console.log(`API REST corriendo en: http://localhost:${server.address().port}`)
+    app.listen(config.port, () => {
+        console.log(`API REST corriendo en: http://localhost:${config.port}`)
     });
 
 });
