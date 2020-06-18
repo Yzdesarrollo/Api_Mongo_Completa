387515d6f86cb557a0b67c54ddf9aef44b1868e8
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-18 16:11:30 -0500

    Creando funciones y llevando el codigo de los metodos al controlador

---
+++ controllers/productController.js
@@ -0,0 +1,74 @@
+'use strict'
+const ProductModel = require('../models/productModel');
+
+function getProducts(req, res){
+    ProductModel.find({}, (err, products) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
+        if (!products) return res.status(404).send({ message: 'No existen los productos' })
+
+        res.status(200).send({ products })
+    })
+}
+
+function getProduct(req, res){
+
+    let productId = req.params.productId
+
+    ProductModel.findById(productId, (err, product) => {
+        if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
+        if (!product) return res.status(404).send({ message: `El producto no existe` })
+
+        res.status(200).send({ product: product })
+    })
+}
+
+function saveProduct(req, res) {
+    //probando si llegan los datos
+    console.log('POST:/api/product');
+    console.log(req.body);
+
+    // instanciando el modelo
+    let products = new ProductModel()
+
+    // propiedades del modelo
+    products.name = req.body.name
+    products.price = req.body.price
+    products.category = req.body.category
+    products.image = req.body.image
+
+    products.save((err, data) => {
+        if (err) return res.status(500).send({ message: `no se pudo guardar los datos: ${err}` })
+        res.status(200).send({ products: data })
+    })
+}
+
+function updateProduct(req, res) {
+    let productId = req.params.productId
+    let body = req.body
+
+    ProductModel.findByIdAndUpdate(productId, body, (err, productUpdated) => {
+        if (err) res.status(500).send({ message: `Error al actualizar el producto: ${err}` })
+        res.status(200).send({ product: productUpdated })
+    })
+}
+
+function deleteProduct(req, res) {
+    let productId = req.params.productId
+
+    ProductModel.findById(productId, (err, product) => {
+        if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` })
+
+        product.remove(err => {
+            if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` })
+            res.status(200).send({ message: 'El producto ha sido eliminado' })
+        })
+    })
+}
+
+module.exports = {
+    getProducts,
+    getProduct,
+    saveProduct,
+    updateProduct,
+    deleteProduct
+}
--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// import Schema o models
-const ProductModel = require('./models/productModel');
-const e = require('express');
+// import 
+const productCtrl= require('./controllers/productController');
 
 // server
 const app = express();
@@ -16,73 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //endpoints
-// Get en general
-app.get('/api/product', (req, res) => {
-
-    ProductModel.find({}, (err, products) => {
-        if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
-        if (!products) return res.status(404).send({ message: 'No existen los productos' })
-
-        res.status(200).send({ products })
-    })
-});
-
-// Get individual
-app.get('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId
-
-    ProductModel.findById(productId, (err, product) => {
-        if (err) return res.status(500).send({ message: `Error al realizar la peticion ${err}` })
-        if (!product) return res.status(404).send({ message: `El producto no existe` })
-
-        res.status(200).send({ product: product })
-    })
-
-});
-
-app.post('/api/product', (req, res) => {
-
-    //probando si llegan los datos
-    console.log('POST:/api/product');
-    console.log(req.body);
-
-    // instanciando el modelo
-    let products = new ProductModel()
-
-    // propiedades del modelo
-    products.name = req.body.name
-    products.price = req.body.price
-    products.category = req.body.category
-    products.image = req.body.image
-
-    products.save((err, data) => {
-        if (err) return res.status(500).send({ message: `no se pudo guardar los datos: ${err}` })
-        res.status(200).send({ products: data })
-    })
-});
-
-app.put('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId
-    let body = req.body
-
-    ProductModel.findByIdAndUpdate(productId, body, (err, productUpdated) => {
-        if(err) res.status(500).send({ message: `Error al actualizar el producto: ${err}` })
-        res.status(200).send({ product: productUpdated })
-    })
-});
-
-app.delete('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId
-
-    ProductModel.findById(productId, (err, product) => {
-        if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` })
-
-        product.remove(err => {
-            if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` })
-            res.status(200).send({ message: 'El producto ha sido eliminado' })
-        })
-    })
-});
+app.get('/api/product', productCtrl.getProducts)
+app.get('/api/product/:productId', productCtrl.getProduct)
+app.post('/api/product', productCtrl.saveProduct)
+app.put('/api/product/:productId', productCtrl.updateProduct)
+app.delete('/api/product/:productId', productCtrl.deleteProduct)
 
 // server connect
 mongoose.connect('mongodb://localhost:27017/productsyzm', (error, res) => {
